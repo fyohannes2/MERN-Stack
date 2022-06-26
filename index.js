e8708e96fa009cd644c23bc000fb5f4c753a3cff
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-26 10:37:07 -0400

    make it better

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const app = express();
+const cors =  require("cors");
 const mongoose = require("mongoose");
+const FriendModel = require("./models/Friends");
+require("dotenv").config();
 
 
 /// DATABASE CONNECTION
 mongoose.connect(
-    " mongodb+srv://admin:abc1234@clusterb.t5btr.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://admin:abc1234@clusterb.t5btr.mongodb.net/?retryWrites=true&w=majority",
     { useNewUrlParser: true }
   );
 
-
-
-app.listen(3001, () => {
+  app.get("/insert", async (req, res) => {
+    const friend = new FriendModel({ name: "Jessic", age: 38 });
+    await friend.save();
+    res.send("Inserted DATA");
+  });
+  
+  app.get("/read", async (req, res) => {
+    FriendModel.find({}, (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+  
+  app.listen(process.env.PORT || 3001, () => {
     console.log("You are connected!");
   });
-
-  
